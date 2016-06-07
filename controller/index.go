b3de5285541fdef1d2a035c08e92612e1836c85d
Roy Dong <i@roydong.com>
2016-06-07 17:13:41 +0800

    fix bug

--- controller/index.go
+++ controller/index.go
@@ -6,25 +6,40 @@ func init()  {
     gmvc.SetAction(func(r *gmvc.Request) *gmvc.Response {
 
         name, _ := r.String("$1")
+
+
+
+        sess := r.Session
+
+        sess.Set("aaa", name)
+
+
+
         return r.HtmlResponse(name, "index/index")
 
     }, `/home/(\w+)`)
 
 
     gmvc.SetAction(func(r *gmvc.Request) *gmvc.Response {
+
+        val, _ := r.Session.Get("aaa").Str()
+
         data := map[string]string{
             "aaa": "aaa",
             "name": "Roy",
             "sex": "男",
+            "sess": val,
         }
         return r.JsonResponse(data)
     }, "/json")
 
 
     gmvc.WSActionMap["ws"] = func(wsm *gmvc.WSMessage) {
+        val, _ := wsm.String("a")
+
         q := map[string]interface{}{
             "aaa": "sss",
-            "value": wsm.Query["a"],
+            "value": val,
         }
         wsm.Send("name", q, nil)
     }
