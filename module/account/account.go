896edd9f42017d69ceebe65f75a7eb6535a30c2b
Roy Dong <i@roydong.com>
2017-10-06 14:43:21 +0800

    .

--- module/account/account.go
+++ module/account/account.go
@@ -1,6 +1,10 @@
 package account
 
-import "dw/poker/context"
+import (
+    "dw/poker/context"
+    "dw/poker/market"
+    "dw/poker/utils"
+)
 
 type Account struct {
 
@@ -14,5 +18,10 @@ func (this *Account) Init(conf *context.Config) error {
 
 func (this *Account) Run(ctx *context.Context) error {
 
+    ok := market.GetExchange("okex/quarter")
+
+    p := ok.LastnAvgPrice(10)
+
+    utils.DebugLog.Write("last 5 avg price %f", p)
     return nil
 }
